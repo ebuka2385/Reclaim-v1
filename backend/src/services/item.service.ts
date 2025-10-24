d4d52cf043371c5daed7fe63055977eaffbb8627
EthanHunt101 <ethandouglashunt@gmail.com>
2025-10-24 11:50:29 -0400

    Added location data to backend 2

--- backend/src/services/item.service.ts
+++ backend/src/services/item.service.ts
@@ -30,7 +30,7 @@ export class ItemService {
       description: data.description,
       status: data.status as PrismaItemStatus,
       userId: data.userId,
-      //location: data.location,
+      location: data.location,
     } });
   }
 
